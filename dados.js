69a18e62f313b40cb77af0e827323b9641bd7c2d
Gabrield819 <gabrield819@gmail.com>
2022-11-22 23:07:14 -0300

    2fast2quick

--- dados.js
+++ dados.js
@@ -1,4 +1,18 @@
 const meusProdutos = [
+    {
+        id: 111,
+        produto: 'cebola',
+        descricao: 'saco com 20kg',
+        valoUnitario: '150',
+        quantidade: '8'
+    },
+    {
+        id: 22222,
+        produto: 'batata',
+        descricao: 'saco com 25kg',
+        valoUnitario: '60',
+        quantidade: '16'
+    }
 ];
 
 export default meusProdutos;
